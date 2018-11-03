f5d48230fa7b0ab3d6100a9676baf65d31c636e6
ilaydaucar <ilaydaucar66@gmail.com>
2018-11-04 00:11:08 +0300

    Order Steps

---
+++ app/src/main/java/com/example/coffee/coffeetime/Barcode.java
@@ -0,0 +1,13 @@
+package com.example.coffee.coffeetime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Barcode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode);
+    }
+}
---
+++ app/src/main/java/com/example/coffee/coffeetime/OrderStep3.java
@@ -0,0 +1,13 @@
+package com.example.coffee.coffeetime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderStep3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_step3);
+    }
+}
