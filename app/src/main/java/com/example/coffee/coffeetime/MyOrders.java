d6cb4c9909f5cad950b8c8c0b9f695816b33ec16
gulsude <gulsudedemircan@gmail.com>
2018-11-15 14:20:01 +0300

    Commit

--- app/src/main/java/com/example/coffee/coffeetime/MyOrders.java
+++ app/src/main/java/com/example/coffee/coffeetime/MyOrders.java
@@ -1,10 +1,13 @@
 package com.example.coffee.coffeetime;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MyOrders extends AppCompatActivity {
 
@@ -21,6 +24,28 @@ public class MyOrders extends AppCompatActivity {
             }
         });
 
+
+
+        Button reorderButton1 = findViewById(R.id.reorderButton1);
+        reorderButton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MyOrders.this, OrderStep3.class));
+            }
+        });
+
+        ImageView imageReorderDescription = (ImageView) findViewById(R.id.imageReorderDescription);
+        imageReorderDescription.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "Reorder same coffee selection and store";
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
 
     }
 }
