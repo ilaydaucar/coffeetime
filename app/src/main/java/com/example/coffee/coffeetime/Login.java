df2a199f1ba55edc96849c29262bf20c836c1dc8
ilaydaucar <ilaydaucar66@gmail.com>
2019-01-08 20:13:23 +0300

    Login Page Updated

--- app/src/main/java/com/example/coffee/coffeetime/Login.java
+++ app/src/main/java/com/example/coffee/coffeetime/Login.java
@@ -1,19 +1,41 @@
 package com.example.coffee.coffeetime;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.coffee.coffeetime.db.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+
+    TextView loginEmail;
+    TextView loginPassword;
     TextView registerInLoginPage;
     Button loginInLoginPage;
+    DatabaseReference mDatabase;
+    String  emailDatabase;
+    String passwordDatabase;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginEmail=findViewById(R.id.loginEmail);
+        loginPassword=findViewById(R.id.loginPassword);
         registerInLoginPage=findViewById(R.id.pleaseRegister);
         registerInLoginPage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,14 +44,44 @@ public class Login extends AppCompatActivity {
                 startActivity(loginToRegister);
             }
         });
+
         loginInLoginPage=findViewById(R.id.loginPageButton);
         loginInLoginPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent loginToMainMenu = new Intent(Login.this,MainMenu.class);
-                startActivity(loginToMainMenu);
+                String password=loginPassword.getText().toString();
+                System.out.println("Mail" + password);
+                mDatabase=FirebaseDatabase.getInstance().getReference().child("users").child(password);
+                mDatabase.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        User user = dataSnapshot.getValue(User.class);
+                        emailDatabase= user.getEmail();
+                        passwordDatabase = user.getPassword();
+                        System.out.println("dataMail" + emailDatabase);
+                        System.out.println("dataPassword" + passwordDatabase);
+
+                        // Log.d("İlayda" , dataSnapshot.getValue().toString());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                if(loginEmail.getText().toString().equals(emailDatabase) &&loginPassword.getText().toString().equals(passwordDatabase)){
+                    Intent loginToMainMenu = new Intent(Login.this,MainMenu.class);
+                    startActivity(loginToMainMenu);
+                }
+                else{
+                    Toast.makeText(getBaseContext(),"Email or Password is incorrect. Please try again", Toast.LENGTH_SHORT).show();
+                }
+
+
+
             }
         });
+
 
     }
 }
--- app/src/main/java/com/example/coffee/coffeetime/db/User.java
+++ app/src/main/java/com/example/coffee/coffeetime/db/User.java
@@ -47,6 +47,7 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
+                "userId='" + userId + '\'' +
                 "name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
                 ", phoneNumber=" + phoneNumber +
