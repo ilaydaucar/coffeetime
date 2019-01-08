6f94a601a504b4740399a1fb8927c265f38e90ed
kaanselim <kaan.selim.7171@gmail.com>
2019-01-09 01:22:52 +0300

    thread

--- app/src/main/java/com/example/coffee/coffeetime/MainActivity.java
+++ app/src/main/java/com/example/coffee/coffeetime/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.coffee.coffeetime;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       /*
         Button register_test = findViewById(R.id.registerButton);
         register_test.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +31,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent_login);
             }
         });
+*/
+       Thread thread = new Thread(){
+           @Override
+           public void run() {
+               try{
+                   sleep(2000);
+                   Log.d("sleep", "run: sleeped 2 seconds");
+               }
+               catch (Exception e) {
+
+               }
+               finally {
+                   Intent intent_login = new Intent(MainActivity.this,Login.class);
+                   startActivity(intent_login);
+               }
+           }
+       };thread.start();
+
+
     }
+
 }
